fba4da37b46905438ad65f14a65f9ba73b52e140
luizgbbosco <luizgbbosco@gmail.com>
2018-08-07 11:04:11 -0300

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { variable } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  valorAtual: string;
+  valorSalvo: string;
+
+  isMouseOver: boolean = false;
+
+  botaoClick(){
+    console.log("botão clicado");
+    alert("Botão Clicado!");
+  }
+  onKeyUp(evento: KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
 }
+
