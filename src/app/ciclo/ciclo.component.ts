81dabae78df9c101ef04ab4550e0ba8092414365
heliohachimine <helio_hachimine_jr@hotmail.com>
2018-08-07 13:57:50 -0300

    ciclo, input, output property, form

---
+++ src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnChanges, OnInit, DoCheck, AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnChanges, OnInit,
+  DoCheck, AfterContentChecked, AfterContentInit, AfterViewChecked,
+  AfterViewInit, OnDestroy {
+
+  @Input() valorInicial: number = 10;
+
+  constructor() {
+    console.log('contructor');
+   }
+
+   ngOnChanges(){
+     console.log('ngOnChanges');
+   }
+   
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
+  }
+
+}
